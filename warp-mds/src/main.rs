7e6b4492dfff3be749b8447a50bcc95e3541d3a4
Rrogntudju <mario.prince@outlook.com>
2020-03-15 13:39:51 -0400

    Progrès

--- warp-mds/src/main.rs
+++ warp-mds/src/main.rs
@@ -1,10 +1,19 @@
 #[tokio::main]
 async fn main() {
-    use warp::Filter;
     use mmds::filters::{get_mds, patch_mds, put_mds};
-    
+    use std::env::args;
+    use std::net::SocketAddr;
+    use warp::Filter;
+
     let api = get_mds().or(patch_mds()).or(put_mds());
 
-    warp::serve(api).run(([127, 0, 0, 1], 7878)).await;
-}
+    let arg = match args().nth(1) {
+        Some(arg) => arg,
+        None => "127.0.0.1:8080".to_string(),
+    };
 
+    match arg.parse::<SocketAddr>() {
+        Ok(addr) => warp::serve(api).run(addr).await,
+        Err(e) => eprintln!("{}", e.to_string()),
+    }
+}
